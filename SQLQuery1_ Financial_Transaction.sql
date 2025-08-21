eeeb09d389fe6a7a2d20ccedf5ac8ae1d7808d6e
Parbati Thapa <parbatithapamagar001@gmail.com>
2025-08-21 22:50:53 +0545

    Initial commit

---
+++ SQLQuery1_ Financial_Transaction.sql
@@ -0,0 +1,54 @@
+﻿USE Financial_transactions_db;
+
+CREATE TABLE Financial_transactions
+(
+    Transaction_id INT PRIMARY KEY,
+    Customer_id INT,
+    Supplier_name VARCHAR(50),
+    Transaction_date DATE,
+    Amount DECIMAL(10, 2),
+    Currency VARCHAR(10)
+);
+
+INSERT INTO Financial_transactions (
+    Transaction_id,
+    Customer_id,
+    Supplier_name,
+    Transaction_Date,
+    Amount,
+    Currency
+)
+VALUES
+(1, 101, 'ABC Corp', '2024-01-15', 1000.00, 'USD'),
+(2, 102, 'XYZ Ltd', '2024-01-20', 1500.50, 'EUR'),
+(3, 103, 'Global Inc', '2024-02-05', 2000.00, 'GBP'),
+(4, 104, 'ABC Corp', '2024-02-10', 500.25, 'USD');
+
+CREATE TABLE Customer_details (
+    Customer_id INT PRIMARY KEY,
+    Customer_name VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(20)
+);
+
+INSERT INTO Customer_details (
+    Customer_id,
+    Customer_name,
+    Email,
+    Phone
+)
+VALUES
+(101, 'John Doe', 'john.doe@example.com', '123-456-7890'),
+(102, 'Jane Smith', 'jane.smith@example.com', '234-567-8901'),
+(103, 'Mike Johnson', 'mike.johnson@example.com', '345-678-9012'),
+(104, 'Emily Davis', 'emily.davis@example.com', '456-789-0123');
+
+
+SELECT * FROM [dbo].[Financial_transactions]
+SELECT * FROM [dbo].[Customer_details]
+
+--JOIN
+SELECT t.*, c.Customer_name, c.Email AS Customer_email, c.Phone AS Customer_Phone
+FROM [Financial_transactions] t
+INNER JOIN [Customer_details] c
+ON t.Customer_id = c.Customer_id;
---
+++ SQLQuery2_Financial_Data_Warehouse.sql
@@ -0,0 +1,56 @@
+USE Financial_data_warehouse
+GO
+
+/****** Object:  Table [dbo].[Financial_transactions]    Script Date: 8/21/2025 9:58:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Financial_transactions](
+	[Transaction_id] [int] NOT NULL,
+	[Customer_id] [int] NULL,
+	[Supplier_name] [varchar](50) NULL,
+	[Transaction_date] [date] NULL,
+	[Amount] [decimal](10, 2) NULL,
+	[Currency] [varchar](10) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Transaction_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+-- ADD Columns in the table
+ALTER TABLE  [dbo].[Financial_transactions] 
+ADD
+	[Customer_name] [varchar](50) NULL,
+	[Customer_email] [varchar](100) NULL,
+	[Customer_phone] [varchar](20) NULL;
+
+
+
+--		TRUNCATE TABLE [dbo].[Financial_transactions]
+
+CREATE TABLE Exchange_rates
+(
+	From_currency VARCHAR(10),
+	To_currency VARCHAR(10),
+	Exchange_rate FLOAT,
+	Effective_date DATE
+);
+
+CREATE TABLE Suppliers
+(
+	Supplier_id INT,
+	Supplier_name VARCHAR(100),
+	Contact_name VARCHAR(100),
+	Phone VARCHAR(25)
+);
+
+SELECT * FROM Financial_transactions
+
+SELECT * FROM Exchange_rates
+
+SELECT * FROM Suppliers
